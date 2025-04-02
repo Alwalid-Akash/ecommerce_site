a21b863f851bbcc8fb46e5bd53f92ec996fa09e6
MD AL WALID <mdalwalid@Mac.fritz.box>
2025-04-03 00:34:50 +0200

    Updated code

--- src/components/HomeItem.jsx
+++ src/components/HomeItem.jsx
@@ -3,19 +3,7 @@ import React from 'react'
 const HomeItem = () => {
   return (
     <>
-      <div className="card" style={{ width: "18rem" }}>
-        <img
-          src={product.image}
-          className="card-img-top"
-          alt={product.title}
-          style={{ height: "200px", objectFit: "cover" }}
-        />
-        <div className="card-body text-center">
-          <h5 className="card-title">{product.title}</h5>
-          <p className="card-text">${product.price}</p>
-          <button className="btn btn-primary">Add to Cart</button>
-        </div>
-      </div>
+
 
     </>
   )
