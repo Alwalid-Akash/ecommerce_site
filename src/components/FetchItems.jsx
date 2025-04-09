bbc4c4e9c4461b4d274ca4bff7dafef590bcb76d
MD AL WALID <alwalidakash@gmail.com>
2025-04-09 23:21:15 +0200

    Updated code6

---
+++ src/components/FetchItems.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Fetchitems = () => {
+  return (
+    <div>Fetchitems</div>
+  )
+}
+
+export default Fetchitems
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,15 +1,14 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import { Outlet } from "react-router-dom"; // ✅ Import Outlet for routing
+import { Outlet } from "react-router-dom";
+
 
 function App() {
   return (
     <>
       <Header />
 
-
       <Outlet />
-
       <Footer />
     </>
   );
